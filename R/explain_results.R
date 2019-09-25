be806446b8e65949ab1a83cfe63518274c383322
Richard Gardiner <rgardiner@accesslex.org>
2019-09-25 15:36:01 -0400

    adding explain_results()

---
+++ R/explain_results.R
@@ -0,0 +1,69 @@
+#' Explaining Results for OLS models
+#' 
+#' This function returns a string for a specific variable that explains in simple terms
+#' the influence of the independent variable on the dependent variable.
+#' 
+#' @param model_name The oLS model created
+#' @param variable The independent variable you want explained
+#' 
+#' @importFrom magrittr %>%
+#' @importFrom dplyr mutate
+#' @importFrom dplyr filter
+#' @importFrom broom tidy
+#' @importFrom tibble as_tibble
+#' @importFrom tidyr spread
+#' @importFrom tidyr unite
+#' 
+#' @export
+#' 
+#' @examples
+#' model1 <- lm(mpg ~ wt, data = mtcars)
+#' model2 <- lm(mpg ~ wt + cyl + hp + wt + disp, data = mtcars)
+#' explain_results(model1, "wt")
+#' explain_results(model2, "cyl")
+explain_results <- function(model_name, variable) {
+  
+  # extracting results
+  results <- broom::tidy(model_name) %>%
+    dplyr::mutate(directionality1 = ifelse(estimate < 0, "negative", "positive"),
+           directionality2 = ifelse(estimate < 0, "decreased", "increased"),
+           absolute_value = round(abs(estimate), 3)) %>%
+    dplyr::filter(term == variable)
+  
+  # dependent variable
+  dv <- names(model_name$model)[1]
+  
+  # independent variables
+  independent_variables <- names(model_name$model)[-1]
+  
+ 
+  
+  
+  
+  # output
+  if(length(independent_variables) < 2) {
+    return(paste0("The variable '", results$term, "' had a ", results$directionality1, 
+                  " relationship with '", dv, 
+                  "'. Where when '", results$term, "' went up by 1 unit, '", dv, "' ",
+                  results$directionality2, " by ", results$absolute_value))
+    
+  } else {
+    
+    # controls
+    controls <- independent_variables %>%
+      tibble::as_tibble() %>%
+      dplyr::mutate(row_id = dplyr::row_number()) %>%
+      dplyr::filter(value != variable) %>% # interactive part
+      tidyr::spread(row_id, value) %>%
+      tidyr::unite("ivs", 1:ncol(.), sep = ", ")
+    
+    
+    return(paste0("The variable '", results$term, "' had a ", results$directionality1, 
+                  " relationship with '", dv, 
+                  "'. Where when '", results$term, "' went up by 1 unit, '", dv, "' ",
+                  results$directionality2, " by ", results$absolute_value,
+                  " while controlling for: ", controls)) 
+    # this last line spits out multiple rows.  That is a problem
+    
+  }
+}
