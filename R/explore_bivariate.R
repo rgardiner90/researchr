297eae7687abce9f2a959d6888e2afdc61902161
Richard Gardiner <rgardiner@accesslex.org>
2019-10-02 12:08:52 -0400

    added feature of a single control variable.

--- R/explore_bivariate.R
+++ R/explore_bivariate.R
@@ -7,23 +7,22 @@
 #' @param data A matrix, data frame, or tibble.
 #' @param dependent A string character of the dependent variable
 #' @param independent A string vector of the different independent variables to test
+#' @param control A string character of one control variable that can be used
 #' @param p_value The significance threshold for a table output (defaults to 0.05). Graph is
 #' fixed at 95 percent confidence intervals
 #' @param type Specifies the type of output request.  either 'graph' or 'table'
-#' @param model_type Specificies whether you have an OLS or logit model.
+#' @param model_type Specificies whether you have an OLS or logit model
 #'
 #' @importFrom magrittr %>%
 #'
 #' @export
 #'
 #' @examples
-#' explore_bivariate(mtcars, "mpg", c("cyl", "disp", "hp", "drat"), p_value = 0.00001)
+#' explore_bivariate(data = mtcars, dependent = "mpg", independent = c("cyl", "disp", "hp", "drat"), control = "gear", p_value = 0.05, type = "table")
 #' explore_bivariate(mtcars, names(mtcars)[1], names(mtcars)[-1])
-explore_bivariate <- function(data, dependent, independent, p_value = 0.05, type = "graph",
+explore_bivariate <- function(data, dependent, independent, control = FALSE,
+                              p_value = 0.05, type = "graph",
                               model_type = "ols") {
-
-  # getting the names of all the variables
-  variables <- colnames(data)
 
   # getting the names of the independnet variables
   independent_text <- independent
@@ -41,53 +40,129 @@ explore_bivariate <- function(data, dependent, independent, p_value = 0.05, type
                       ifelse(tolower(model_type) == "logit", "binomial",
                              "please select either 'ols' or 'logit'"))
 
-  # running the models
-  models <- lapply(iv_list, function(x) {
-    broom::tidy(glm(substitute(dv ~ i, list(dv = as.name(dependent_text),
-                                            i = as.name(x))),
-                    data = data,
-                    family = modelType))
-  })
+  if(control == FALSE) {
 
 
-  # getting output
-  if(type == "graph") {
+    # running the models
+    models <- lapply(iv_list, function(x) {
+      broom::tidy(glm(substitute(dv ~ i, list(dv = as.name(dependent_text),
+                                              i = as.name(x))),
+                      data = data,
+                      family = modelType))
+    })
 
-    results <- models %>%
-      tibble::tibble() %>%
-      tidyr::unnest(cols = c(.)) %>%
-      dplyr::filter(term != "(Intercept)") %>%
-      dplyr::select(term, estimate, std.error) %>%
-      dplyr::mutate(lower = (estimate - (std.error * 1.96)),
-                    upper = (estimate + (std.error * 1.96)),
-                    term = forcats::fct_reorder(term, estimate),
-                    significance = ifelse(estimate > 0 & lower > 0, "positive",
-                                          ifelse(estimate < 0 & upper < 0, "negative", "not significant"))) %>%
-      ggplot2::ggplot(ggplot2::aes(x = term, y = estimate,
-                                   ymin = lower, ymax = upper, color = significance)) +
-      ggplot2::theme_minimal() +
-      ggplot2::geom_hline(yintercept = 0.0, color = "red", lty = 2) +
-      ggplot2::geom_point() +
-      ggplot2::geom_linerange() +
-      ggplot2::labs(title = "Results are from bivariate tests, not a single model.",
-                    caption = "Graph results show a 95% confidence interval",
-                    x = "", y = "Coefficient") +
-      ggplot2::coord_flip() +
-      ggplot2::scale_color_manual(values = c("red2", "gray", "#7CAE00"))
 
-    return(results)
+    # getting output
+    if(type == "graph") {
 
-  } else if(type == "table") {
+      data <- models %>%
+        tibble::tibble() %>%
+        tidyr::unnest(cols = c(.)) %>%
+        dplyr::filter(term != "(Intercept)") %>%
+        dplyr::select(term, estimate, std.error) %>%
+        dplyr::mutate(lower = (estimate - (std.error * 1.96)),
+                      upper = (estimate + (std.error * 1.96)),
+                      term = forcats::fct_reorder(term, estimate),
+                      significance = ifelse(estimate > 0 & lower > 0, "positive",
+                                            ifelse(estimate < 0 & upper < 0,
+                                                   "negative", "not significant")))
 
-    results <- models %>%
-      tibble::tibble() %>%
-      tidyr::unnest(cols =c(.)) %>%
-      dplyr::filter(term != "(Intercept)") %>%
-      dplyr::mutate(significance = ifelse(p.value < p_value, 1, 0))
+      cols <- c("negative" = "red2", "not significant" = "gray",
+                "positive" = "#7CAE00")
 
-    return(results)
+      results <- data %>%
+        ggplot2::ggplot(ggplot2::aes(x = term, y = estimate,
+                                     ymin = lower, ymax = upper, color = significance)) +
+        ggplot2::theme_minimal() +
+        ggplot2::geom_hline(yintercept = 0.0, color = "red", lty = 2) +
+        ggplot2::geom_point() +
+        ggplot2::geom_linerange() +
+        ggplot2::labs(title = "Results are from bivariate tests, not a single model.",
+                      caption = "Graph results show a 95% confidence interval",
+                      x = "", y = "Coefficient") +
+        ggplot2::coord_flip() +
+        ggplot2::scale_color_manual(values = cols)
+
+      return(results)
+
+    } else if(type == "table") {
+
+      results <- models %>%
+        tibble::tibble() %>%
+        tidyr::unnest(cols =c(.)) %>%
+        dplyr::filter(term != "(Intercept)") %>%
+        dplyr::mutate(significance = ifelse(p.value < p_value, 1, 0))
+
+      return(results)
+    } else {
+      return("Please select either 'graph' or 'table' for type")
+    }
+
   } else {
-    return("Please select either 'graph' or 'table' for type")
+
+
+    control_text <- control
+    control_variable <- data[, control_text]
+
+
+    # running the models
+    models <- lapply(iv_list, function(x) {
+      broom::tidy(glm(substitute(dv ~ i + control_var,
+                                 list(dv = as.name(dependent_text),
+                                      control_var = as.name(control_text),
+                                      i = as.name(x))),
+                      data = data,
+                      family = modelType))
+    })
+
+    # getting output
+    if(type == "graph") {
+
+      data <- models %>%
+        tibble::tibble() %>%
+        tidyr::unnest(cols = c(.)) %>%
+        dplyr::filter(term != "(Intercept)",
+                      term != control) %>%
+        dplyr::select(term, estimate, std.error) %>%
+        dplyr::mutate(lower = (estimate - (std.error * 1.96)),
+                      upper = (estimate + (std.error * 1.96)),
+                      term = forcats::fct_reorder(term, estimate),
+                      significance = ifelse(estimate > 0 & lower > 0, "positive",
+                                            ifelse(estimate < 0 & upper < 0,
+                                                   "negative", "not significant")))
+
+      cols <- c("negative" = "red2", "not significant" = "gray",
+                "positive" = "#7CAE00")
+
+      results <- data %>%
+        ggplot2::ggplot(ggplot2::aes(x = term, y = estimate,
+                                     ymin = lower, ymax = upper, color = significance)) +
+        ggplot2::theme_minimal() +
+        ggplot2::geom_hline(yintercept = 0.0, color = "red", lty = 2) +
+        ggplot2::geom_point() +
+        ggplot2::geom_linerange() +
+        ggplot2::labs(title = "Results are from bivariate tests, not a single model.",
+                      caption = "Graph results show a 95% confidence interval",
+                      x = "", y = "Coefficient") +
+        ggplot2::coord_flip() +
+        ggplot2::scale_color_manual(values = cols)
+
+      return(results)
+
+    } else if(type == "table") {
+
+      results <- models %>%
+        tibble::tibble() %>%
+        tidyr::unnest(cols =c(.)) %>%
+        dplyr::filter(term != "(Intercept)",
+                      term != control) %>%
+        dplyr::mutate(significance = ifelse(p.value < p_value, 1, 0))
+
+      return(results)
+    } else {
+      return("Please select either 'graph' or 'table' for type")
+    }
+
+
   }
 }
-
